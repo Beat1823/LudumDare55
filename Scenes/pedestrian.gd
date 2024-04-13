5cb55b7e83ca8893c45cd135706e78246f1aa5e1
ThymoB <thymoboog@hotmail.com>
2024-04-13 21:04:50 +0200

    added scuffed ped

--- Scenes/pedestrian.gd
+++ Scenes/pedestrian.gd
@@ -6,11 +6,14 @@ var move_direction : Vector2 = Vector2.ZERO
 
 @onready var timer = $MovementTimer
 
+var is_alive: bool = true
+
 func _ready():
 	select_new_direction()
 	timer.start(walk_time)
 
 func _on_movement_timer_timeout():
+	$AnimatedSprite2D.play("idle")
 	select_new_direction()
 	timer.start(walk_time)
 	
@@ -19,13 +22,15 @@ func select_new_direction():
 		randi_range(-1,1),
 		randi_range(-1,1)
 	)
+	$AnimatedSprite2D.play("walking")
 	
 func _physics_process(_delta):
 	velocity = move_direction * move_speed
 	move_and_slide()
 
-
 func _on_car_detector_body_entered(body):
-	get_node("CollisionShape2D").disabled = true
-	queue_free()
+	$CarDetector/CollisionShape2D.disabled = true
+	$AnimatedSprite2D.play("dead")
 	PlayerData.pedestrian_count -= 1
+	is_alive = false
+	
