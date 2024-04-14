aa578fe0c12fb6aa9ed5a7756b6c14b183a0459c
ThymoB <thymoboog@hotmail.com>
2024-04-14 09:13:15 +0200

    added autodestroy to spawned sounds

--- scripts/SoundManager.gd
+++ scripts/SoundManager.gd
@@ -7,6 +7,7 @@ func playSound2D(sound:AudioStream, vol = 0, pitch = 1, randVol = 3, randPitch =
 	player.pitch_scale = pitch + randf_range(-randPitch, randPitch)
 	add_child(player)
 	player.play()
+	player.connect("finished", player.queue_free)
 
 func playSound3D(sound:AudioStream, pos, vol = 0, pitch = 1, randVol = 3, randPitch = 0.1):
 	var player = AudioStreamPlayer2D.new()
@@ -16,6 +17,7 @@ func playSound3D(sound:AudioStream, pos, vol = 0, pitch = 1, randVol = 3, randPi
 	add_child(player)
 	player.position = pos
 	player.play()
+	player.connect("finished", player.queue_free)
 
 func playRandomSound3D(sounds:Array[AudioStreamOggVorbis], pos, vol = 0, pitch = 1, randVol = 3, randPitch = 0.1):
 	playSound3D(sounds.pick_random(), pos, vol, pitch, randVol, randPitch)
