265343006d281468549a06ddc423a8cb08862d8f
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-04-14 18:47:52 +0200

    end game fade in

--- Scenes/GameUI.gd
+++ Scenes/GameUI.gd
@@ -3,19 +3,24 @@ extends Control
 @export var winSounds:Array[AudioStreamOggVorbis]
 @export var loseSounds:Array[AudioStreamOggVorbis]
 
+@onready var anim_player = get_node("AnimationPlayer")
+
 func showEndGameScreen(won:bool = false):
-	$CanvasLayer/EndGameScreen.visible = true
 	if won:
-		$CanvasLayer/EndGameScreen/Header.text = "[center]You bloody did it!"
-		$CanvasLayer/EndGameScreen/Body.text = "[center]The demon hath been summoned"
+		anim_player.play("fade")
+		await anim_player.animation_finished	
+		$EndGameCanvas/EndGameScreen.visible = true
+		$EndGameCanvas/EndGameScreen/Header.text = "[center]You bloody did it!"
+		$EndGameCanvas/EndGameScreen/Body.text = "[center]The demon hath been summoned"
 		SoundManager.playRandomSound2D(winSounds, -8, 1, 0, 0)
-	else:
-		$CanvasLayer/EndGameScreen/Header.text = "[center]You lost!"
-		$CanvasLayer/EndGameScreen/Body.text = "[center]You got caught with no blood"
+	else:		
+		$EndGameCanvas/EndGameScreen.visible = true
+		$EndGameCanvas/EndGameScreen/Header.text = "[center]You lost!"
+		$EndGameCanvas/EndGameScreen/Body.text = "[center]You got caught with no blood"
 		SoundManager.playRandomSound2D(loseSounds, -8, 1, 0, 0)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://UI/start_screen.tscn")
 
 func _on_pentagram_progress_update(progress):
-	$CanvasLayer/PentagramPaintedText.text = "[center]Pentagram painted:\n[b]" + str(roundi(clampf(progress, 0, 1) * 100)) + "%"
+	$BloodCanvas/PentagramPaintedText.text = "[center]Pentagram painted:\n[b]" + str(roundi(clampf(progress, 0, 1) * 100)) + "%"
